d6d8bd1d1f637ab7161e1a931da87868fd737d8c
Dahanne Anthony <Anthony.Dahanne@softwareag.com>
2014-04-22 20:07:35 +0000

    TAB-4485 : moving management-core content to a sub folder
    svn path=/projects/management-common/trunk/; revision=43529

---
+++ management-core/src/main/java/org/terracotta/management/application/DefaultApplication.java
@@ -0,0 +1,32 @@
+/*
+ * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package org.terracotta.management.application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * A default {@link Application} subclass that adds the commonly used
+ * resources and providers.
+ * @author Ludovic Orban
+ */
+public class DefaultApplication extends Application {
+
+  /**
+   * Get a default set of resource and provider classes.
+   * @return a default set of classes.
+   */
+  @Override
+  public Set<Class<?>> getClasses() {
+    return new HashSet<Class<?>>() {{
+      add(DefaultExceptionMapper.class);
+      add(ResourceRuntimeExceptionMapper.class);
+      add(WebApplicationExceptionMapper.class);
+    }};
+  }
+
+}
---
+++ management-core/src/test/java/org/terracotta/management/resource/services/UtilsTest.java
@@ -0,0 +1,27 @@
+/*
+ * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+
+package org.terracotta.management.resource.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author brandony
+ */
+public class UtilsTest {
+  @Test
+  public void testTrimToNull() {
+    String nullStr = null;
+    Assert.assertNull(Utils.trimToNull(nullStr));
+
+    String emptyStr = "";
+    Assert.assertNull(Utils.trimToNull(emptyStr));
+
+    String nonNullStr = "stuff";
+    Assert.assertNotNull(Utils.trimToNull(nonNullStr));
+    Assert.assertEquals(nonNullStr, Utils.trimToNull(nonNullStr));
+  }
+}
