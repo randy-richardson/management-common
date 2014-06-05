ab639e839783fcdf94a76a1483f8e72bf9700e85
Dahanne Anthony <Anthony.Dahanne@softwareag.com>
2014-06-05 20:43:34 +0000

    BS-239 : DefaultApplication classes don't need to extend javax.ws.core.Application , since they're composed into an Application class, but never used directly- this cleans things up and fixes an issue with websessiosn not being able to be deployed on Jboss (jboss eagerly tries to load all Application classes, if it finds more than 1, it stops deployment)
    svn path=/projects/management-common/trunk/; revision=43844

--- management-common-v2/src/main/java/org/terracotta/management/application/DefaultApplicationV2.java
+++ management-common-v2/src/main/java/org/terracotta/management/application/DefaultApplicationV2.java
@@ -7,22 +7,19 @@ package org.terracotta.management.application;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.ws.rs.core.Application;
-
 import org.terracotta.management.resource.services.AgentsResourceServiceImplV2;
 
 /**
- * A default {@link Application} subclass that adds the commonly used
+ * A class that adds the commonly used
  * resources and providers.
  * @author Ludovic Orban
  */
-public class DefaultApplicationV2 extends Application {
+public class DefaultApplicationV2 {
 
   /**
    * Get a default set of resource and provider classes.
    * @return a default set of classes.
    */
-  @Override
   public Set<Class<?>> getClasses() {
     return new HashSet<Class<?>>() {{
         add(DefaultExceptionMapperV2.class);
