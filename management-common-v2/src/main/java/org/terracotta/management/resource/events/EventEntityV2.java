8338ff38b5f765eac28b51ae497aa2d0ff13ad91
Dahanne Anthony <Anthony.Dahanne@softwareag.com>
2014-06-11 21:29:54 +0000

    TAB-4713 : creating the base class for events entities : EventEntityV2
    svn path=/projects/management-common/trunk/; revision=43871

---
+++ management-common-v2/src/main/java/org/terracotta/management/resource/events/EventEntityV2.java
@@ -0,0 +1,73 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.management.resource.events;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.terracotta.management.resource.VersionedEntityV2;
+
+/**
+ * A {@link org.terracotta.management.resource.VersionedEntityV2} representing a topology event
+ * from the management API.
+ *
+ * @author Ludovic Orban
+ */
+public class EventEntityV2 extends VersionedEntityV2 {
+
+  private String agentId;
+  private String sourceId;
+  private String event;
+  private String targetNodeId;
+  private String targetJmxId;
+
+  private final Map<String, Object> rootRepresentables = new HashMap<String, Object>();
+
+
+  public String getSourceId() {
+    return sourceId;
+  }
+
+  public void setSourceId(String sourceId) {
+    this.sourceId = sourceId;
+  }
+
+  public String getEvent() {
+    return event;
+  }
+
+  public void setEvent(String event) {
+    this.event = event;
+  }
+
+  public String getTargetNodeId() {
+    return targetNodeId;
+  }
+
+  public void setTargetNodeId(String targetNodeId) {
+    this.targetNodeId = targetNodeId;
+  }
+
+  public String getTargetJmxId() {
+    return targetJmxId;
+  }
+
+  public void setTargetJmxId(String targetJmxId) {
+    this.targetJmxId = targetJmxId;
+  }
+
+  @Override
+  public String getAgentId() {
+    return agentId;
+  }
+
+  @Override
+  public void setAgentId(String agentId) {
+    this.agentId = agentId;
+  }
+
+  public Map<String, Object> getRootRepresentables() {
+    return rootRepresentables;
+  }
+}
