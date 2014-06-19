16b55599c0b032ef6b393cfb1eb47cb63f171b56
hung huynh <hhuynh-unknown@terracottatech.com>
2014-06-19 18:28:39 +0000

    add message and stackTrace
    svn path=/projects/management-common/trunk/; revision=43933

--- management-common-v2/src/main/java/org/terracotta/management/resource/ExceptionEntityV2.java
+++ management-common-v2/src/main/java/org/terracotta/management/resource/ExceptionEntityV2.java
@@ -1,5 +1,23 @@
 package org.terracotta.management.resource;
 
 public class ExceptionEntityV2 extends AbstractEntityV2{
-
+  private String message;
+  private String stackTrace;
+  
+  public String getMessage() {
+    return message;
+  }
+  public void setMessage(String message) {
+    this.message = message;
+  }
+  public String getStackTrace() {
+    return stackTrace;
+  }
+  public void setStackTrace(String stackTrace) {
+    this.stackTrace = stackTrace;
+  }
+  
+  public String toJSON() {
+    return String.format("{\"message\" : \"%s\", \"stackTrace\" : \"%s\"}", message, stackTrace);
+  }
 }
