2fd1c03f39b8dba40da1e5cc43b9a1fcd3fe931b
Dahanne Anthony <Anthony.Dahanne@softwareag.com>
2014-09-12 19:13:30 +0000

    TAB-5096 : using nn.getBytes(Charset.forName(UTF-8)) for events
    svn path=/projects/management-common/trunk/; revision=45010

--- management-common-resources-v2/src/main/java/org/terracotta/management/resource/services/events/TerracottaEventOutput.java
+++ management-common-resources-v2/src/main/java/org/terracotta/management/resource/services/events/TerracottaEventOutput.java
@@ -2,7 +2,7 @@ package org.terracotta.management.resource.services.events;
 
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.server.ChunkedOutput;
-
+import java.nio.charset.Charset;
 /**
  * This class exists to overcome an defect seen in Chrome where the Jersey EventOutput uses "\n\n" as the
  * chunkDelimeter. The symptom would be a net::ERR_INVALID_CHUNKED_ENCODING on the Javascript EventSource receiving a
@@ -12,6 +12,6 @@ import org.glassfish.jersey.server.ChunkedOutput;
  */
 public class TerracottaEventOutput extends ChunkedOutput<OutboundEvent> {
   public TerracottaEventOutput() {
-    super("\r\n".getBytes());
+    super("\n\n".getBytes(Charset.forName("UTF-8")));
   }
 }
