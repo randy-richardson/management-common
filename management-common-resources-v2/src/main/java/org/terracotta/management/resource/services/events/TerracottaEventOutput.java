52261229be622c3a063b29c63fc53f069b4acf57
Orban Ludovic <Ludovic.Orban@terracottatech.com>
2014-12-23 19:30:37 +0000

    TAB-5100: fix SSE performance and concurrency bug
    svn path=/projects/management-common/trunk/; revision=45972

--- management-common-resources-v2/src/main/java/org/terracotta/management/resource/services/events/TerracottaEventOutput.java
+++ management-common-resources-v2/src/main/java/org/terracotta/management/resource/services/events/TerracottaEventOutput.java
@@ -2,16 +2,87 @@ package org.terracotta.management.resource.services.events;
 
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.server.ChunkedOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.charset.Charset;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * This class exists to overcome an defect seen in Chrome where the Jersey EventOutput uses "\n\n" as the
  * chunkDelimeter. The symptom would be a net::ERR_INVALID_CHUNKED_ENCODING on the Javascript EventSource receiving a
  * Server-Sent Event and that event would be malformed.
- * 
+ * <p/>
+ * Another problem this class solves is that it makes {@link #write} thread-safe: it should be but isn't in
+ * ChunkedOutput. To keep performance intact despite thread-safety, messages are flushed only once every
+ * {@link #BATCH_SIZE} times. To prevent events lingering in the queue waiting for a {@link #BATCH_SIZE} quorum,
+ * a timer performs a flush every 100ms.
+ *
  * @author gkeim
+ * @author Ludovic Orban
  */
 public class TerracottaEventOutput extends ChunkedOutput<OutboundEvent> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(TerracottaEventOutput.class);
+
+  public static final int BATCH_SIZE = 32;
+  public static final long TIMER_INTERVAL = 100L;
+
+  private final Field flushingField;
+  private int counter = 0;
+
   public TerracottaEventOutput() {
     super("\n\n".getBytes(Charset.forName("UTF-8")));
+
+    Timer flushTimer = new Timer("sse-flush-timer", true);
+    flushTimer.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        try {
+          flush();
+        } catch (IOException ioe) {
+          LOG.warn("Error flushing SSE from timer", ioe);
+        }
+      }
+    }, TIMER_INTERVAL, TIMER_INTERVAL);
+
+    try {
+      flushingField = ChunkedOutput.class.getDeclaredField("flushing");
+      flushingField.setAccessible(true);
+    } catch (NoSuchFieldException nsfe) {
+      throw new RuntimeException(nsfe);
+    }
   }
+
+  private void switchOffAutoFlushing(boolean flushing) {
+    try {
+      flushingField.set(this, flushing);
+    } catch (IllegalAccessException iae) {
+      throw new RuntimeException(iae);
+    }
+  }
+
+  @Override
+  public synchronized void write(OutboundEvent chunk) throws IOException {
+    boolean flush = ++counter % BATCH_SIZE == 0;
+
+    if (!flush) {
+      switchOffAutoFlushing(true);
+    }
+    try {
+      super.write(chunk);
+    } finally {
+      if (!flush) {
+        switchOffAutoFlushing(false);
+      }
+    }
+  }
+
+  private synchronized void flush() throws IOException {
+    super.write(null);
+  }
+
 }
