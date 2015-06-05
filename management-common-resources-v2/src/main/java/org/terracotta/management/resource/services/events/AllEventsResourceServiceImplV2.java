d6100eb74790eb4ef9447485b51b652df28fbb5a
Gary Keim <gary.keim@softwareag.com>
2015-06-05 20:26:38 +0000

    Refactored to Jersey SseBroadcaster for more thread-safety
    svn path=/projects/management-common/trunk/; revision=47570

--- management-common-resources-v2/src/main/java/org/terracotta/management/resource/services/events/AllEventsResourceServiceImplV2.java
+++ management-common-resources-v2/src/main/java/org/terracotta/management/resource/services/events/AllEventsResourceServiceImplV2.java
@@ -4,13 +4,13 @@
 package org.terracotta.management.resource.services.events;
 
 import org.glassfish.jersey.media.sse.OutboundEvent;
+import org.glassfish.jersey.media.sse.SseBroadcaster;
 import org.glassfish.jersey.media.sse.SseFeature;
+import org.glassfish.jersey.server.ChunkedOutput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terracotta.management.ServiceLocator;
 import org.terracotta.management.resource.events.EventEntityV2;
-
-import java.io.IOException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -31,9 +31,48 @@ public class AllEventsResourceServiceImplV2 {
   private static final Logger LOG = LoggerFactory.getLogger(AllEventsResourceServiceImplV2.class);
 
   private final EventServiceV2 eventService;
+  private final SseBroadcaster broadcaster;
 
   public AllEventsResourceServiceImplV2() {
     this.eventService = ServiceLocator.locate(EventServiceV2.class);
+    this.broadcaster = new Broadcaster();
+  }
+
+  private class Broadcaster extends SseBroadcaster {
+    @Override
+    public void onException(final ChunkedOutput<OutboundEvent> chunkedOutput, final Exception exception) {
+      LOG.debug("Error writing to OutputEvent", exception);
+    }
+
+    @Override
+    public void onClose(final ChunkedOutput<OutboundEvent> chunkedOutput) {
+      eventService.unregisterEventListener((EventServiceListener) chunkedOutput);
+    }
+  }
+
+  public class EventServiceListener extends TerracottaEventOutput implements EventServiceV2.EventListener {
+    @Override
+    public void onEvent(EventEntityV2 eventEntity) {
+      OutboundEvent.Builder eventBuilder = new OutboundEvent.Builder();
+      eventBuilder.reconnectDelay(100);
+      eventBuilder.mediaType(MediaType.APPLICATION_JSON_TYPE);
+      eventBuilder.name(EventEntityV2.class.getSimpleName());
+      eventBuilder.data(EventEntityV2.class, eventEntity);
+      OutboundEvent event = eventBuilder.build();
+
+      AllEventsResourceServiceImplV2.this.broadcaster.broadcast(event);
+
+      if (LOG.isDebugEnabled()) {
+        LOG.debug(String.format("Event dispatched: {AgentId: %s, Type: %s, ApiVersion: %s, Representables: %s}",
+                                eventEntity.getAgentId(), eventEntity.getType(), eventEntity.getApiVersion(),
+                                eventEntity.getRootRepresentables()));
+      }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+      LOG.debug("Error when waiting for management events.", throwable);
+    }
   }
 
   @GET
@@ -41,50 +80,11 @@ public class AllEventsResourceServiceImplV2 {
   public TerracottaEventOutput getServerSentEvents(@Context UriInfo info, @QueryParam("localOnly") boolean localOnly) {
     LOG.debug(String.format("Invoking AllEventsResourceServiceImplV2.getServerSentEvents: %s", info.getRequestUri()));
 
-    final TerracottaEventOutput eventOutput = new TerracottaEventOutput();
-    eventService.registerEventListener(new EventServiceV2.EventListener() {
-      @Override
-      public void onEvent(EventEntityV2 eventEntity) {
-        OutboundEvent.Builder eventBuilder = new OutboundEvent.Builder();
-        eventBuilder.reconnectDelay(100);
-        eventBuilder.mediaType(MediaType.APPLICATION_JSON_TYPE);
-        eventBuilder.name(EventEntityV2.class.getSimpleName());
-        eventBuilder.data(EventEntityV2.class, eventEntity);
-        OutboundEvent event = eventBuilder.build();
+    EventServiceListener eventOutput = new EventServiceListener();
 
-        try {
-          eventOutput.write(event);
-          if(LOG.isDebugEnabled()) {
-            LOG.debug(String.format("Event dispatched: {AgentId: %s, Type: %s, ApiVersion: %s, Representables: %s}",
-                eventEntity.getAgentId(),
-                eventEntity.getType(),
-                eventEntity.getApiVersion(),
-                eventEntity.getRootRepresentables()));
-          }
-        } catch (Exception e) {
-          eventService.unregisterEventListener(this);
-          try {
-            eventOutput.close();
-          } catch (IOException ioe) {
-            LOG.warn("Error when closing the event output.", ioe);
-          }
-        }
-      }
-
-      @Override
-      public void onError(Throwable throwable) {
-        LOG.warn("Error when waiting for management events.", throwable);
-
-        eventService.unregisterEventListener(this);
-        try {
-          eventOutput.close();
-        } catch (IOException ioe) {
-          LOG.warn("Error when closing the event output.", ioe);
-        }
-      }
-    }, localOnly);
+    broadcaster.add(eventOutput);
+    eventService.registerEventListener(eventOutput, localOnly);
 
     return eventOutput;
   }
-
 }
