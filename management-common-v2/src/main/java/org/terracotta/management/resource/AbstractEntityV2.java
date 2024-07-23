b32b6ff41c94a77aab1bb4950cc5253f80e67f40
Nishchay <nishchay.naresh@gmail.com>
2024-07-23 16:18:45 +0530

    updating copyright header
    (cherry picked from commit 0f536bbcab0d0d6eb071ddc09d23526bbb220c74)

--- management-common-v2/src/main/java/org/terracotta/management/resource/AbstractEntityV2.java
+++ management-common-v2/src/main/java/org/terracotta/management/resource/AbstractEntityV2.java
@@ -1,6 +1,7 @@
 /*
  * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
  * notice. All rights reserved.
+ * Copyright Super iPaaS Integration LLC, an IBM Company 2024
  */
 
 package org.terracotta.management.resource;
